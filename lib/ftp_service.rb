95982d06e37ca265f795f90114125da5f924ebbb
Justin Blake <justin@megablaix.com>
2010-03-08 17:44:20 -0500

    Update usage docs/comments for FtpService

--- lib/ftp_service.rb
+++ lib/ftp_service.rb
@@ -4,15 +4,28 @@ require 'tempfile_helper'
 # Class for dealing with a service that acts like a web service, except
 # over FTP. Meaning an xml request is uploaded as a file, and the
 # response xml is downloaded as another file.
+#
+# Typical usage:
+# 
+#   FtpService.open('host', 'user', 'pass') do |service|
+#     path = '/the/remote/path'
+#     service.write_request("#{path}/request.xml", '<foo>bar</foo>')
+#     response = service.read_response("#{path}/response.xml")
+#   end
+#
+# or (the sucky way):
+#
+#   path = '/the/remote/path'
+#   service = FtpService.new('host', 'user', 'pass')
+#   service.write_request("#{path}/request.xml", '<foo>bar</foo>')
+#   response = service.read_response("#{path}/response.xml")
+#   service.close
 class FtpService
   
   # Open a connection to the FTP server and return an FtpService object.
-  # The object must be closed explicitely.
-  #
-  #   ftp_service = FtpService.open(host, user, pass)
-  #   ftp_service.write_request(path, request)
-  #   response = ftp_service.read_response(path)
-  #   ftp_service.close
+  # The object must be closed explicitely. See FtpServier#open for a
+  # better way to do this that will automatically close the connection,
+  # even if an exception is raised.
   def initialize(host, user, pass)
     @ftp = Net::FTP.open(host, user, pass)
   end
@@ -20,11 +33,6 @@ class FtpService
   # Open a connection and pass an FtpService instance to the block. The
   # instance will be closed when the block finishes, or when an
   # exception is raised.
-  #
-  #   FtpService.open(host, user, pass) do |ftp_service|
-  #     ftp_service.write_request(path, request)
-  #     response = ftp_service.read_response(path)
-  #   end
   def self.open(host, user, pass)
     instance = new(host, user, pass)
     begin
@@ -34,12 +42,15 @@ class FtpService
     end
   end
   
-  # Write the +request+ to the +remote_path+ on the FTP server.
+  # Write `request` to a local temp file and upload it to `remote_path`
+  # on the FTP server.
   def write_request(request, remote_path)
     tmp = TempfileHelper.write(request, 'request')
     @ftp.puttextfile(tmp.path, remote_path)
   end
   
+  # Download the file at `remote_path` from the FTP server to a  local
+  # temp file and return its contents.
   def read_response(remote_path)
     TempfileHelper.read('response') do |tmp|
       @ftp.gettextfile(remote_path, tmp.path)
